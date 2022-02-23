d8e0d71db8bbc510be8830fc32f707299a693ed0
Greg Lind <greglind@gregs-air.lan>
2022-02-23 12:08:03 -0800

    fix migrations

--- home/migrations/0003_aboutpage_artistspage.py
+++ home/migrations/0003_aboutpage_artistspage.py
@@ -13,16 +13,6 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='AboutPage',
-            fields=[
-                ('page_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='wagtailcore.page')),
-            ],
-            options={
-                'abstract': False,
-            },
-            bases=('wagtailcore.page',),
-        ),
-        migrations.CreateModel(
             name='ArtistsPage',
             fields=[
                 ('page_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='wagtailcore.page')),
