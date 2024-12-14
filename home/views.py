f867d4aaef85da57309d6915fbec93e574774453
Greg Lind <gwlind@gmail.com>
2024-12-14 09:19:45 -0800

    update

--- home/views.py
+++ home/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from wagtail.models import Page
 
 def home_view(request):
-    latest_articles = Page.objects.filter(show_in_menus='false').order_by('-first_published_at')[:3]  # Retrieving the latest 3 news articles
+    latest_articles = Page.objects.live().filter(show_in_menus=False).order_by('-first_published_at')[:3]  # Retrieving the latest 3 live news articles
     print(latest_articles)
     return render(request, 'home/welcome_page.html', {'latest_articles': latest_articles})
+
+
+
+
